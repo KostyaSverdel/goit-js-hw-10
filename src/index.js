59f943210ca8acebad4acdffff02763af825cea3
KostyaSverdel <109824269+KostyaSverdel@users.noreply.github.com>
2023-01-19 02:35:35 +0200

    addNotiflix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+const inputSearchBox = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+addInlineStyles();
+
+inputSearchBox.addEventListener('input', debounce(searchInput, DEBOUNCE_DELAY));
